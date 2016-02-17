b82d55489bedfd06128bd4c50c8beba438e882e0
vipul1546 <vipul.t220@gmail.com>
2016-02-17 16:41:40 +0530

    final commit

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    #image = models.ImageField(upload_to='/images',blank=True,null=True)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class News(models.Model):
+	source = models.CharField(max_length=200)
+	news_detail = models.TextField()
+
+	def __str__(self):
+		return self.source
